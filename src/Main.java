c7c87b7a0aef090b354ef5877dcf1a763991b779
Igor Dokai <igordokai@outlook.com>
2023-07-04 17:53:42 -0300

    building the logical structure

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,48 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        /*
+        static boolean checkRes(String res) {
+            if (res.equals("s")) {
+                return true;
+            } else {
+                return false;
+            }
+        };
+        */
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        boolean runProgram = false;
+        boolean resCheck = false;
+        String methodsRes;
+
+        double a;
+        double b;
+        double c;
+
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            do {
+                System.out.println("Calculadora de Baskhara :");
+                System.out.println("Estabeleça o valor da variável A");
+                a = sc.nextDouble();
+                System.out.println("Estabeleça o valor da variável B");
+                b = sc.nextDouble();
+                System.out.println("Estabeleça o valor da variável C");
+                c = sc.nextDouble();
+                System.out.printf("Os valores selecionados foram %n A: %.2f%n B: %.2f%n C: %.2f%n", a, b, c);
+                System.out.print("Você digitou as variáveis corretamente? (s/n)");
+                methodsRes = sc.next();
+                //checkRes(methodsRes);
+            } while (resCheck == false);
+            System.out.print("Fora do Loop");
+        } while (runProgram == true);
+
+        sc.close();
     }
 }
