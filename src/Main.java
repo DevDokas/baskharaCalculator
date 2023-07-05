f0c35a0969eddc4e86238cd34152415ac9429531
Dokas <iguinhogms@gmail.com>
2023-07-05 00:37:50 -0300

    code finalized

--- src/Main.java
+++ src/Main.java
@@ -4,53 +4,59 @@ import java.util.Scanner;
 
 public class Main {
 
-    static double calculate(double A, double B, double C) {
-        double delta = Math.pow(B, 2)-4*A*C;
-        double result = B;
-
-        return result;
-    }
 
     public static void main(String[] args) {
 
-
-
-        /*
-        static boolean checkRes(String res) {
-            if (res.equals("s")) {
-                return true;
-            } else {
-                return false;
-            }
-        };
-        */
-
         boolean runProgram = false;
-        boolean resCheck = false;
         String methodsRes;
 
         double a;
         double b;
         double c;
 
+
         Scanner sc = new Scanner(System.in);
 
         do {
-            do {
+
                 System.out.println("Calculadora de Baskhara :");
+
                 System.out.println("Estabeleça o valor da variável A");
                 a = sc.nextDouble();
+
                 System.out.println("Estabeleça o valor da variável B");
                 b = sc.nextDouble();
+
                 System.out.println("Estabeleça o valor da variável C");
                 c = sc.nextDouble();
+
                 System.out.printf("Os valores selecionados foram %n A: %.2f%n B: %.2f%n C: %.2f%n", a, b, c);
+                double delta = b*b-4*a*c;
+
                 System.out.print("Você digitou as variáveis corretamente? (s/n)");
                 methodsRes = sc.next();
-                //checkRes(methodsRes);
-            } while (resCheck == false);
-            System.out.print("Fora do Loop");
-        } while (runProgram == true);
+
+                if (methodsRes.equals("s")) {
+
+                    if (delta < 0) {
+                        System.out.println("Não possui raiz real");
+                        runProgram = true;
+                    } else if (delta == 0) {
+                        double root = -b/(2*a);
+                        System.out.println("A equação possui uma raiz real : " + root);
+                        runProgram = true;
+                    } else {
+                        double rootMax = (-b + Math.sqrt(delta)) / (2 * a);
+                        double rootMin = (-b - Math.sqrt(delta)) / (2 * a);
+                        System.out.println("A equação possui dois valores reais:");
+                        System.out.println("Raiz positiva:" + rootMax);
+                        System.out.println("Raiz negativa:" + rootMin);
+                        runProgram = true;
+                    }
+                }
+        } while (!runProgram);
+
+        System.out.println("Fora do Loop");
 
         sc.close();
     }
